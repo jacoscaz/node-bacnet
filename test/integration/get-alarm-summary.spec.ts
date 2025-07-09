2f615594bf5366f886f85c22c09f30d5cf3919e5
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-07-09 22:24:04 +0200

    chore: refactors tests to use async/await

--- test/integration/get-alarm-summary.spec.ts
+++ test/integration/get-alarm-summary.spec.ts
@@ -4,19 +4,16 @@ import assert from 'node:assert'
 import * as utils from './utils'
 
 test.describe('bacnet - getAlarmSummary integration', () => {
-	test('should return a timeout error if no device is available', (t) => {
-		return new Promise((resolve) => {
-			const client = new utils.BacnetClient({ apduTimeout: 200 })
-			client.getAlarmSummary(
-				{ address: '127.0.0.2' },
-				{},
-				(err, value) => {
-					assert.strictEqual(err.message, 'ERR_TIMEOUT')
-					assert.strictEqual(value, undefined)
-					client.close()
-					resolve()
-				},
-			)
-		})
+  test('should return a timeout error if no device is available', async (t) => {
+		const client = new utils.BacnetClient({ apduTimeout: 200 })
+    try {
+      client.getAlarmSummary(
+        { address: '127.0.0.2' },
+        {},
+      )
+    } catch (err) {
+			assert.strictEqual((err as Error).message, 'ERR_TIMEOUT')
+			client.close()
+    }
 	})
 })
--- test/integration/get-enrollment-summary.spec.ts
+++ test/integration/get-enrollment-summary.spec.ts
@@ -4,20 +4,17 @@ import assert from 'node:assert'
 import * as utils from './utils'
 
 test.describe('bacnet - getEnrollmentSummary integration', () => {
-	test('should return a timeout error if no device is available', (t) => {
-		return new Promise((resolve) => {
-			const client = new utils.BacnetClient({ apduTimeout: 200 })
-			client.getEnrollmentSummary(
-				{ address: '127.0.0.2' },
-				0,
-				{ notificationClassFilter: 5 },
-				(err, value) => {
-					assert.strictEqual(err.message, 'ERR_TIMEOUT')
-					assert.strictEqual(value, undefined)
-					client.close()
-					resolve()
-				},
-			)
-		})
+	test('should return a timeout error if no device is available', async (t) => {
+	  const client = new utils.BacnetClient({ apduTimeout: 200 })
+    try {
+      client.getEnrollmentSummary(
+        { address: '127.0.0.2' },
+        0,
+        { notificationClassFilter: 5 },
+      )
+    } catch (err) { 
+      assert.strictEqual((err as Error).message, 'ERR_TIMEOUT')
+			client.close()      
+    }
 	})
 })
