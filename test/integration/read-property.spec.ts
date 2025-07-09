748d775cfc5400df597555220950f0bfe52a1da2
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-07-09 22:19:27 +0200

    chore: refactors tests to use async/await

--- test/integration/read-property.spec.ts
+++ test/integration/read-property.spec.ts
@@ -4,21 +4,18 @@ import assert from 'node:assert'
 import * as utils from './utils'
 
 test.describe('bacnet - readProperty integration', () => {
-	test('should return a timeout error if no device is available', (t) => {
-		return new Promise((resolve) => {
-			const client = new utils.BacnetClient({ apduTimeout: 200 })
-			client.readProperty(
-				{ address: '127.0.0.2' },
-				{ type: 8, instance: 44301 },
-				28,
-				{},
-				(err, value) => {
-					assert.strictEqual(err.message, 'ERR_TIMEOUT')
-					assert.strictEqual(value, undefined)
-					client.close()
-					resolve()
-				},
-			)
-		})
+	test('should return a timeout error if no device is available', async (t) => {
+	  const client = new utils.BacnetClient({ apduTimeout: 200 })
+    try {
+      client.readProperty(
+        { address: '127.0.0.2' },
+        { type: 8, instance: 44301 },
+        28,
+        {},
+      )
+    } catch (err) {
+			assert.strictEqual((err as Error).message, 'ERR_TIMEOUT')
+			client.close()
+    }
 	})
 })
